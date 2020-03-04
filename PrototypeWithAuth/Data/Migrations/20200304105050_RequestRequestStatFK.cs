9b18b5ee3a8b7198569cae716be8f2c8aecb50bb
Adina Silberberg <adinasilberberg@gmail.com>
2020-03-04 12:57:57 +0200

    refresh adina db from request status id

--- PrototypeWithAuth/Data/Migrations/20200304105050_RequestRequestStatFK.cs
+++ PrototypeWithAuth/Data/Migrations/20200304105050_RequestRequestStatFK.cs
@@ -2,10 +2,9 @@
 
 namespace PrototypeWithAuth.Data.Migrations
 {
-    public partial class RequestStatInRequests : Migration
+    public partial class RequestRequestStatFK : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
-
         {
             migrationBuilder.AddColumn<int>(
                 name: "RequestStatusID",
@@ -13,9 +12,9 @@ namespace PrototypeWithAuth.Data.Migrations
                 nullable: true);
 
             migrationBuilder.CreateIndex(
-                    name: "IX_Requests_RequestStatusID",
-                    table: "Requests",
-                    column: "RequestStatusID");
+                name: "IX_Requests_RequestStatusID",
+                table: "Requests",
+                column: "RequestStatusID");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_Requests_RequestStatuses_RequestStatusID",
@@ -26,11 +25,8 @@ namespace PrototypeWithAuth.Data.Migrations
                 onDelete: ReferentialAction.Restrict);
         }
 
-
-
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
             migrationBuilder.DropForeignKey(
                 name: "FK_Requests_RequestStatuses_RequestStatusID",
                 table: "Requests");
