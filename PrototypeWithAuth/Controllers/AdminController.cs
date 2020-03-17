8e64d4f4d27746baddfb7f56f87c6418008da30a
Faige Weinfeld <faige@centarix.biz>
2020-03-17 13:07:49 +0200

    functioning  administrator controller allowing for new users to be registered with specified role

--- PrototypeWithAuth/Controllers/AdminController.cs
+++ PrototypeWithAuth/Controllers/AdminController.cs
@@ -3,16 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrototypeWithAuth.AppData;
+using PrototypeWithAuth.Data;
+using PrototypeWithAuth.Models;
+using PrototypeWithAuth.ViewModels;
 
 namespace PrototypeWithAuth.Controllers
 {
     public class AdminController : Controller
     {
-        [Authorize(Roles = "Admin")]
-        public IActionResult Index()
+        //private readonly ApplicationDbContext _context;
+        private SignInManager<ApplicationUser> _signManager;
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        public AdminController(/*ApplicationDbContext context,*/ UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signManager,RoleManager<IdentityRole> roleManager)
         {
-            return View();
+           // _context = context;
+            _userManager = userManager;
+            _signManager = signManager;
+            _roleManager = roleManager;
+        }
+      
+        [HttpGet]
+        public IActionResult RegisterUser()
+            
+        {
+            var roles = _roleManager.Roles; // get the roles from db and have displayed sent to view model
+            RegisterUserViewModel registerUserViewModel = new RegisterUserViewModel
+            {
+                Roles = roles
+
+            };
+            return View(registerUserViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterUser(RegisterUserViewModel registerUserViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = registerUserViewModel.Email,
+                    Email = registerUserViewModel.Email,
+                    FirstName = registerUserViewModel.FirstName,
+                    LastName = registerUserViewModel.LastName
+                };
+            
+                var result = await _userManager.CreateAsync(user, registerUserViewModel.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, registerUserViewModel.Role);
+                    await _signManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "ApplicationUsers");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(registerUserViewModel);
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetHomeView()
+        {
+            return View("~/Views/Home/IndexAdmin.cshtml");
         }
     }
 }
---
+++ PrototypeWithAuth/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using PrototypeWithAuth.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+
+
+namespace PrototypeWithAuth.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+        public IEnumerable<IdentityRole> Roles { get; set; }
+        
+        [Required]
+        public string Role { get; set; }
+
+    }
+}
