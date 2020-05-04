3055f904e509aa322ffa2e479c1f368703e6cc3c
Faige Weinfeld <faige@centarix.biz>
2020-05-04 14:58:59 +0300

    locations now has property place and fk for request

---
+++ PrototypeWithAuth/Data/Migrations/20200504112614_LocationInstanceRequestFKAndPlaceProp.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrototypeWithAuth.Data.Migrations
+{
+    public partial class LocationInstanceRequestFKAndPlaceProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Place",
+                table: "LocationInstances",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RequestID",
+                table: "LocationInstances",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocationInstances_RequestID",
+                table: "LocationInstances",
+                column: "RequestID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LocationInstances_Requests_RequestID",
+                table: "LocationInstances",
+                column: "RequestID",
+                principalTable: "Requests",
+                principalColumn: "RequestID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LocationInstances_Requests_RequestID",
+                table: "LocationInstances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LocationInstances_RequestID",
+                table: "LocationInstances");
+
+            migrationBuilder.DropColumn(
+                name: "Place",
+                table: "LocationInstances");
+
+            migrationBuilder.DropColumn(
+                name: "RequestID",
+                table: "LocationInstances");
+        }
+    }
+}
