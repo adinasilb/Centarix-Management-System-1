8984ddc84610064de7e9bef36e73a81b20f62ce1
Adina Silberberg <adinasilberberg@gmail.com>
2020-06-29 10:52:03 +0300

    started to separate

--- PrototypeWithAuth/Models/Request.cs
+++ PrototypeWithAuth/Models/Request.cs
@@ -60,6 +60,7 @@ namespace PrototypeWithAuth.Models
         ///[DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = true)]
         [Display(Name = "Price")]
         public double Cost { get; set; } //this is always shekel no matter what currency is
+                                         //
         public string Currency { get; set; }
 
         [Required]
