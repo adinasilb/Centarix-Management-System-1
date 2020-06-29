36b6cf8e1ac9624ad06a527b307e607896eb6768
Adina Silberberg <adinasilberberg@gmail.com>
2020-06-29 13:28:57 +0300

    paged list

--- PrototypeWithAuth/Controllers/RequestsController.cs
+++ PrototypeWithAuth/Controllers/RequestsController.cs
@@ -209,7 +209,7 @@ namespace PrototypeWithAuth.Controllers
             {
                 onePageOfProducts = await RequestsPassedIn.Include(r => r.ParentRequest).Include(r => r.Product.ProductSubcategory)
                     .Include(r => r.Product.Vendor).Include(r => r.RequestStatus).Include(r => r.UnitType).Include(r => r.SubUnitType).Include(r => r.SubSubUnitType)
-                    .ToPagedListAsync(pageNumber, 25);
+                    .ToPagedListAsync(pageNumber, 3);
 
                 onePageOfProducts.OrderByDescending(opop => opop.ArrivalDate).Where(opop => opop.RequestStatusID == 5); // display by arrivaldate if recieved
 
