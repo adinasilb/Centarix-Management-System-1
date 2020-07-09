831615f6d0da93d5f1379d6ca51fc4c6422c5b62
Adina Silberberg <adinasilberberg@gmail.com>
2020-07-09 11:15:38 +0300

    tried to fix the x.pagedlist

--- PrototypeWithAuth/ViewModels/ApplicationUserListViewModel.cs
+++ PrototypeWithAuth/ViewModels/ApplicationUserListViewModel.cs
@@ -1,5 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using PrototypeWithAuth.Models;
+using PrototypeWithAuth;
+using PrototypeWithAuth.ViewModels;
+using PrototypeWithAuth.Areas;
+using PrototypeWithAuth.Areas.Identity;
+using PrototypeWithAuth.Areas.Identity.Pages;
+using PrototypeWithAuth.Areas.Identity.Pages.Account;
+using PrototypeWithAuth.Areas.Identity.Pages.Account.Manage;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
