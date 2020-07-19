95f7477f1e51d8d7b244c5b5fc1ad9cfa1c254aa
danimanikatz <51416372+danimanikatz@users.noreply.github.com>
2020-07-19 11:31:39 +0300

    added vendor comment table in database

---
+++ PrototypeWithAuth/Data/Migrations/20200719082941_AddedVendorComment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrototypeWithAuth.Data.Migrations
+{
+    public partial class AddedVendorComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VendorComments",
+                columns: table => new
+                {
+                    VendorCommentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationUserID = table.Column<string>(nullable: true),
+                    VendorID = table.Column<int>(nullable: false),
+                    CommentText = table.Column<string>(nullable: true),
+                    CommentTimeStamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VendorComments", x => x.VendorCommentID);
+                    table.ForeignKey(
+                        name: "FK_VendorComments_AspNetUsers_ApplicationUserID",
+                        column: x => x.ApplicationUserID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_VendorComments_Vendors_VendorID",
+                        column: x => x.VendorID,
+                        principalTable: "Vendors",
+                        principalColumn: "VendorID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VendorComments_ApplicationUserID",
+                table: "VendorComments",
+                column: "ApplicationUserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VendorComments_VendorID",
+                table: "VendorComments",
+                column: "VendorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VendorComments");
+        }
+    }
+}
---
+++ PrototypeWithAuth/Models/VendorComment.cs
@@ -0,0 +1,33 @@
+﻿using PrototypeWithAuth.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace PrototypeWithAuth.Models
+{
+    public class VendorComment
+    {
+        [Key]
+        public int VendorCommentID { get; set; }
+        public string ApplicationUserID { get; set; } //this is the owner of the request - do we have every received request have its own reciever?
+
+        public int VendorID { get; set; }
+        public Vendor Vendor { get; set; }
+
+        [ForeignKey("ApplicationUserID")]
+        public ApplicationUser ApplicationUser { get; set; }
+        public string CommentText { get; set; }
+
+        [DataType(DataType.Date)]
+        private DateTime commentTimeStamp = DateTime.Now; //should this be readonly -ADINA
+        public DateTime CommentTimeStamp
+        {
+            get => commentTimeStamp;
+            set { }
+        }
+    }
+}
