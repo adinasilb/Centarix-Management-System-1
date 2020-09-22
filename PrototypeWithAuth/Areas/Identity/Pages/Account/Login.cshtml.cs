e350dbc9c929e8ad9c89004f15679d676ee6edf1
DeborahShulman <shulman.deborah1@gmail.com>
2020-09-22 14:27:37 +0300

    vendor create edits

--- PrototypeWithAuth/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PrototypeWithAuth/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -110,56 +110,6 @@ namespace PrototypeWithAuth.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private void fillInTimekeeperMissingDays(ApplicationUser user)
-        {
-                DateTime nextDay = user.LastLogin.AddDays(1);
-                while (nextDay.Date <= DateTime.Today)
-                {
-                    if (nextDay.DayOfWeek != DayOfWeek.Friday && nextDay.DayOfWeek != DayOfWeek.Saturday)
-                    {
-                        var existentHours = _context.EmployeeHours.Where(eh => eh.EmployeeID == user.Id && eh.Date.Date == nextDay.Date).FirstOrDefault();
-                        if (existentHours == null)
-                        {
-                            EmployeeHours employeeHours = new EmployeeHours
-                            {
-                                EmployeeID = user.Id,
-                                Date = nextDay.Date
-                            };
-                            _context.Update(employeeHours);
-                        }
-                    }
-                nextDay = nextDay.AddDays(1);
-                 
-             }
-            _context.SaveChanges();
-        }
 
-        private void fillInOrderLate(ApplicationUser user)
-        { 
-            if (user.LastLogin.Date != DateTime.Now.Date)
-            {
-                var lateOrders = _context.Requests.Where(r => r.ApplicationUserCreatorID == user.Id).Where(r => r.RequestStatusID == 2)
-                    .Where(r => r.ParentRequest.OrderDate.AddDays(r.ExpectedSupplyDays ?? 0).Date >= user.LastLogin.Date && r.ParentRequest.OrderDate.AddDays(r.ExpectedSupplyDays ?? 0).Date < DateTime.Today)
-                    .Include(r => r.Product).ThenInclude(p => p.Vendor).Include(r => r.ParentRequest);
-                foreach (var request in lateOrders)
-                {
-                    RequestNotification requestNotification = new RequestNotification();
-                    requestNotification.RequestID = request.RequestID;
-                    requestNotification.IsRead = false;
-                    requestNotification.RequestName = request.Product.ProductName;
-                    requestNotification.ApplicationUserID = request.ApplicationUserCreatorID;
-                    requestNotification.Description = "should have arrived " + request.ParentRequest.OrderDate.AddDays(request.ExpectedSupplyDays ?? 0).ToString("dd/MM/yyyy");
-                    requestNotification.NotificationStatusID = 1;
-                    requestNotification.TimeStamp = DateTime.Now;
-                    requestNotification.Controller = "Requests";
-                    requestNotification.Action = "NotificationsView";
-                    requestNotification.OrderDate = request.ParentRequest.OrderDate;
-                    requestNotification.Vendor = request.Product.Vendor.VendorEnName;
-                    _context.Update(requestNotification);
-                }
-                _context.SaveChanges();
-
-            }
-        }
     }
 }
