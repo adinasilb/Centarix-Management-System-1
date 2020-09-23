99f51bc9119a390b33551f2818be59a124643ae0
Adina Silberberg <adinasilberberg@gmail.com>
2020-09-23 11:11:30 -0400

    add selected button shows when checked

---
+++ PrototypeWithAuth/wwwroot/js/accounting.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+	$(".form-check.accounting-select").on("change", function (e) {
+		var addToSelectedButton = $("#add-to-selected");
+		if ($(".form-check.accounting-select .form-check-input:checked").length) {
+			console.log("is checked");
+			if (addToSelectedButton.hasClass("hidden")) {
+				console.log("has class hidden");
+				addToSelectedButton.removeClass("hidden");
+			}
+		}
+		else {
+			console.log("is not checked");
+			if (!addToSelectedButton.hasClass("hidden")) {
+				console.log("does not have class hidden");
+				addToSelectedButton.addClass("hidden");
+			}
+		}
+	});
+});
