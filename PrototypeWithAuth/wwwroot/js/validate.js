8c87b00f1d495b6db2139af9c005e7338d5f6d50
DeborahShulman <shulman.deborah1@gmail.com>
2020-10-11 11:32:08 +0300

    got all the validation working

--- PrototypeWithAuth/wwwroot/js/validate.js
+++ PrototypeWithAuth/wwwroot/js/validate.js
@@ -1,89 +1,139 @@
 ﻿$(function () {
-	jQuery.validator.setDefaults({
-		ignore: "",
 
-	});
 	$.validator.addMethod("selectRequired", function (value, element) {
 		console.log("value" + value);
-		return this.optional(element)|| value != '';
+		return  $(element).val() != "";
 	}, 'Field is required');
-    $('.addItemForm').validate({ // initialize the Plugin
-			rules: {
-				"Request.CatalogNumber": {
-					required: true,
-					number: true
-					},
-			"data[data-activates][select-options-parentlist]":"required",
-			},
+
+	$('.mdb-select').change(function () {
+		console.log("mdb focus");
+		$(this).valid();
+	});
+	$('.next-tab').click(function () {
+		var valid = $("#myForm").valid();
+		console.log("valid form: " + valid)
+		if (!valid) {
+			$('.next-tab').prop("disabled", true);
+		}
+		else {
+			$('.next-tab').prop("disabled", false);
+		}
+		
+	});
+	$('#myForm').submit(function (e) {
+		$.validator.setDefaults({
+			ignore: []
+		});
+		var valid = $("#myForm").valid();
+
+		console.log("valid form: " + valid)
+		if (!valid) {
+			e.preventDefault();
+		}
+		$.validator.setDefaults({
+			ignore:':not(select:hidden, input:visible, textarea:visible)'
+		});
+	});
+		$.validator.setDefaults({
+			ignore: ':not(select:hidden, input:visible, textarea:visible)',
+			errorPlacement: function (error, element) {
+				if (element.hasClass('select-dropdown')) {
+					error.insertAfter(element);
+				} else {
+					error.insertAfter(element);
+				}
+			}
+		});
+	var addItemTab1 = {
+		"Request.Product.ProductName": "required",
+		"Request.CatalogNumber": {
+			required: true,
+			number: true,
+			min: 1
+		},
+		"Request.Product.ProductSubcategory.ParentCategoryID": "selectRequired",
+		"Request.Product.ProductSubcategoryID": "selectRequired",
+		"Request.SubProject.ProjectID": "selectRequired",
+		"Request.SubProjectID": "selectRequired",
+		"Request.Product.VendorID": "selectRequired",
+		"Request.ParentQuote.QuoteNumber": {
+			required: true,
+			number: true,
+			min: 1
+		},
+		"Request.ExpectedSupplyDays": {
+			min: 0
+		},
+		"Request.Warranty": {
+			min: 0
+		},
+		"Request.URL": {
+			//url: true
+		},
+		"Request.ExchangeRate": {
+			required: true,
+			number: true,
+			min: 1
+		},
+		"Request.Unit": {
+			required: true,
+			number: true,
+			min: 1
+		},
+		"Request.Cost": {
+			required: false,
+			number: true,
+			min: 1
+		},
+		"sum-dollars": {
+			required: true,
+			number: true,
+			min: 1
+		},
+		"Request.UnitTypeID": "selectRequired",
+	};
+	var addItemTab2 = {
+
+		"Request.ExchangeRate": {
+			required: true,
+			number: true,
+			min: 1
+		},
+		"Request.Unit": {
+			required: true,
+			number: true,
+			min: 1
+		},
+		"Request.Cost": {
+			required: false,
+			number: true,
+			min: 1
+		},
+		"sum-dollars": {
+			required: true,
+			number: true,
+			min: 1
+		},
+		"Request.UnitTypeID": "selectRequired",
+
+	};
+	$('.addItemForm').validate({ // initialize the Plugin
+
+		rules: addItemTab1,
+
 			messages: {
       
-			}
-		
+		}
+	
+	
     //submitHandler: function (form) {
     //    alert('valid form submitted');
     //    return false;
     //}
-});
+	});
+
+
 
 });
-$.fn.validateItemTab = function () {
-	$(".request-price-tab").prop("disabled", true);
-	$(".request-location-tab").prop("disabled", true);
-	$(".request-order-tab").prop("disabled", true);
 
-	console.log("in $.fn.validateItemTab");
-	//valid = $("#parentlist").attr('aria-invalid');
-	//if (valid == "true" || $("#parentlist").val() == "") {
-	//	console.log("valid: " + valid);
-	//	return;
-	//}
-	valid = $("#Request_Product_ProductName").attr('aria-invalid');
-	if (valid == "true" || $("#Request_Product_ProductName").val() == "") {
-		console.log("valid: " + valid);
-		return;
-	}
-	valid = $("#sublist").attr('aria-invalid');
-	if (valid == "true" || $("#sublist").val() == "") {
-		return;
-	}
-	valid = $("#Request_SubProject_ProjectID").attr('aria-invalid');
-	if (valid == "true" || $("#Request_SubProject_ProjectID").val() == "") {
-		return;
-	}
-	console.log("valid1: " + valid);
-	valid = $("#SubProject").attr('aria-invalid');
-	if (valid == "true" || $("#SubProject").val() == "") {
-		return;
-	}
-	valid = $("#vendorList").attr('aria-invalid');
-	if (valid == "true" || $("#vendorList").val() == "") {
-		return;
-	}
-	valid = $("#Request_Warranty").attr('aria-invalid');
-	if (valid == "true") {
-		return;
-	}
-	valid = $("#Request_ParentQuote_QuoteDate").attr('aria-invalid');
-	if (valid == "true" || $("#Request_ParentQuote_QuoteDate").val() == "") {
-		return;
-	}
-	valid = $("#Request_ParentQuote_QuoteNumber").attr('aria-invalid');
-	if (valid == "true" || $("#Request_ParentQuote_QuoteNumber").val() == "") {
-		return;
-	}
-	valid = $("#Request_ExpectedSupplyDays").attr('aria-invalid');
-	if (valid == "true") {
-		return;
-	}
-	valid = $("#Request_CatalogNumber").attr('aria-invalid');
-	if (valid == "true" || $("#Request_CatalogNumber").val() == "") {
-		return;
-	}
-	if (valid == "false" || valid == undefined) {
-		console.log("made it here!!")
-		$(".request-price-tab").prop("disabled", false);
-		$(".request-location-tab").prop("disabled", false);
-		$(".request-order-tab").prop("disabled", false);
-	}
-	return valid;
-}
+
