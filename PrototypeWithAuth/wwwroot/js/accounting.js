50b825b413711ec72f02d6630075362d10c5f53e
Adina Silberberg <adinasilberberg@gmail.com>
2020-10-12 13:27:50 -0400

    troubleshooting pay selected modals

--- PrototypeWithAuth/wwwroot/js/accounting.js
+++ PrototypeWithAuth/wwwroot/js/accounting.js
@@ -1,16 +1,27 @@
 ﻿$(function () {
 	$(".form-check.accounting-select").on("change", function (e) {
-		var addToSelectedButton = $("#add-to-selected");
-		if ($(".form-check.accounting-select .form-check-input:checked").length) {
-			if (addToSelectedButton.hasClass("hidden")) {
-				addToSelectedButton.removeClass("hidden");
-			}
-		}
-		else {
-			if (!addToSelectedButton.hasClass("hidden")) {
-				addToSelectedButton.addClass("hidden");
-			}
-		}
+		console.log("checked box");
+		//var addToSelectedButton = $("#add-to-selected");
+		//var paySelectedButton = $("#pay-selected");
+		//var selectedButton;
+		//if (addToSelectedButton) {
+		//	console.log('in first if');
+		//	selectedButton = addToSelectedButton;
+		//}
+		//else if (paySelectedButton) {
+		//	console.log('in else if');
+		//	selectedButton = paySelectedButton;
+		//}
+		//if ($(".form-check.accounting-select .form-check-input:checked").length) {
+		//	if (selectedButton.hasClass("hidden")) {
+		//		selectedButton.removeClass("hidden");
+		//	}
+		//}
+		//else {
+		//	if (!selectedButton.hasClass("hidden")) {
+		//		selectedButton.addClass("hidden");
+		//	}
+		//}
 	});
 
 });
