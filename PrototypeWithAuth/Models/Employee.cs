c4b6bf3c60727a8014814470547ee3c5fb442917
DeborahShulman <shulman.deborah1@gmail.com>
2020-10-14 14:59:52 +0300

    employee id

--- PrototypeWithAuth/Models/Employee.cs
+++ PrototypeWithAuth/Models/Employee.cs
@@ -40,13 +40,12 @@ namespace PrototypeWithAuth.Models
         public int VacationDays { get; set; }
         [Display(Name = "Job Title")]
         public string JobTitle { get; set; }
-   
+        [Display(Name = "ID Number")]
+        public string IDNumber { get; set; }
         public int DegreeID { get; set; }
         [ForeignKey("DegreeID")]
         public Degree Degree { get; set; }
-        //public string Degree { get; set; }
-        [Display(Name = "ID Number")]
-        public int IDNumber { get; set; }
+
         [Display(Name = "Relationship Status")]    
         public int MaritalStatusID { get; set; }
         [ForeignKey("MaritalStatusID")]
