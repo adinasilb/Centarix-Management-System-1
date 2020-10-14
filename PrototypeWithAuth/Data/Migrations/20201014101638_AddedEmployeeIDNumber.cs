d3e50aeae3f848a107a6b26dd9e7cf8212769c30
DeborahShulman <shulman.deborah1@gmail.com>
2020-10-14 14:59:43 +0300

    add employee id

---
+++ PrototypeWithAuth/Data/Migrations/20201014101638_AddedEmployeeIDNumber.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrototypeWithAuth.Data.Migrations
+{
+    public partial class AddedEmployeeIDNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VendorBuisnessID",
+                table: "Vendors",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IDNumber",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VendorBuisnessID",
+                table: "Vendors",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IDNumber",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
