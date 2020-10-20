8200c532e9d9efe9732eace33ad88598e54158bb
DeborahShulman <shulman.deborah1@gmail.com>
2020-10-20 13:12:08 +0300

    add location modal validation

--- PrototypeWithAuth/wwwroot/js/validate.js
+++ PrototypeWithAuth/wwwroot/js/validate.js
@@ -25,6 +25,40 @@ $(function () {
 			},
 		}
 	});
+	$('.addLocationForm').validate({
+		rules: {
+			"LocationInstance.LocationInstanceName": "required",
+			"LocationInstance.LocationTypeID": {
+				required: true,
+				min:1,
+			},
+			"LocationInstances[0].Height" : {
+				required: true,
+				number: true,
+				min: 1,
+				integer: true
+			},
+			"LocationInstances[0].Width": {
+				required: true,
+				number: true,
+				min: 1,
+				integer: true
+			},
+			"LocationInstances[1].Height": {
+				required: true,
+				number: true,
+				min: 1,
+				integer: true
+			},
+			"LocationInstances[2].Height": {
+				required: true,
+				number: true,
+				min: 1,
+				integer: true
+			},
+			"LocationInstances[2].LocationTypeID": "selectRequired"
+		}
+	});
 	$('.reorderForm').validate({
 		rules: {
 			
