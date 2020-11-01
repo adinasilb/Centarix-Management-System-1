148a58ce9a34fc229bf68579377dff97770859a5
DeborahShulman <shulman.deborah1@gmail.com>
2020-11-01 15:13:36 +0200

    create clibration view

---
+++ PrototypeWithAuth/ViewModels/CreateCalibrationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PrototypeWithAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototypeWithAuth.ViewModels
+{
+    public class CreateCalibrationViewModel
+    {
+        public String ProductDescription { get; set; }
+        public Repair Repair { get; set; }
+        public List<InternalCalibration> InternalCalibration {get; set;}
+        public List<ExternalCalibration> ExternalCalibrations { get; set; }
+        public List<Calibration> PastCalibrations { get; set; }
+        public int RequestID { get; set; }
+    }
+}
