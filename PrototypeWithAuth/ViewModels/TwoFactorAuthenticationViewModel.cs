884c8416cc34f1935d9d2bd5cd641474fb2e500a
DeborahShulman <shulman.deborah1@gmail.com>
2020-11-03 08:58:44 +0200

    two factor changes

---
+++ PrototypeWithAuth/ViewModels/TwoFactorAuthenticationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototypeWithAuth.ViewModels
+{
+    public class TwoFactorAuthenticationViewModel
+    {
+        [Required]
+        [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Verification Code")]
+        public string Code { get; set; }
+
+    }
+}
