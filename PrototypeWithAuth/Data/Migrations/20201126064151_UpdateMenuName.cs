100a434ace155703e568aa965a638c26cdd98215
DeborahShulman <shulman.deborah1@gmail.com>
2020-11-26 08:43:46 +0200

    menu item name change

---
+++ PrototypeWithAuth/Data/Migrations/20201126064151_UpdateMenuName.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrototypeWithAuth.Data.Migrations
+{
+    public partial class UpdateMenuName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Menus",
+                keyColumn: "menuID",
+                keyValue: 1,
+                column: "MenuDescription",
+                value: "Requests");
+
+            migrationBuilder.UpdateData(
+                table: "Menus",
+                keyColumn: "menuID",
+                keyValue: 3,
+                column: "MenuDescription",
+                value: "Operations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Menus",
+                keyColumn: "menuID",
+                keyValue: 1,
+                column: "MenuDescription",
+                value: "OrdersAndInventory");
+
+            migrationBuilder.UpdateData(
+                table: "Menus",
+                keyColumn: "menuID",
+                keyValue: 3,
+                column: "MenuDescription",
+                value: "Operation");
+        }
+    }
+}
