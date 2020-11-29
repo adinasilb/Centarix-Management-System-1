94b8c18692e4df0eb7fe7834bbff207c94088c07
DeborahShulman <shulman.deborah1@gmail.com>
2020-11-29 11:23:00 +0200

    separate validatin scripts out to each file indivisually

---
+++ PrototypeWithAuth/wwwroot/js/Validation/EditQuoteDetailsFormValidation.js
@@ -0,0 +1,16 @@
+﻿
+$('.editQuoteDetails').validate({
+	rules: {
+		"Request.ParentQuote.QuoteNumber": {
+			required: true,
+			number: true,
+			min: 1
+		},
+		"Request.ExpectedSupplyDays": {
+			min: 0,
+			integer: true
+		},
+		"QuoteFileUpload": "required"
+	},
+
+});
---
+++ PrototypeWithAuth/wwwroot/js/Validation/ReorderFormValidation.js
@@ -0,0 +1,34 @@
+﻿$('.reorderForm').validate({
+	rules: {
+
+		"Request.Unit": {
+			required: true,
+			number: true,
+			min: 1,
+			integer: true
+		},
+		"Request.SubUnit": {
+			required: true,
+			number: true,
+			min: 1,
+			integer: true
+		},
+		"Request.SubSubUnit": {
+			required: true,
+			number: true,
+			min: 1,
+			integer: true
+		},
+		"Request.Cost": {
+			required: true,
+			number: true,
+			min: 1
+		},
+
+		"Request.UnitTypeID": "selectRequired",
+		"Request.SubUnitTypeID": "selectRequired",
+		"Request.SubSubUnitTypeID": "selectRequired"
+
+	},
+
+});
