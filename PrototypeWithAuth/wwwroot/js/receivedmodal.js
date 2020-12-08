fa044ebd3d413c1d370f9d4c313b9a589db18554
DeborahShulman <shulman.deborah1@gmail.com>
2020-12-08 11:38:05 +0200

    fixed the location recieved modal design

--- PrototypeWithAuth/wwwroot/js/receivedmodal.js
+++ PrototypeWithAuth/wwwroot/js/receivedmodal.js
@@ -183,23 +183,24 @@
 	$(".visual-locations td").on("click", function () {
 		if ($(this).has("i").length) {
 
-			var locationInstanceId = $(this).children("input").first().attr("liid");
-
+			var locationInstanceId = $(this).children('div').first().children("input").first().attr("liid");
 			var lip = $(".liid." + locationInstanceId);
 			console.log("lip val: " + lip.val());
 			if (lip.val() == "true") {
 				console.log("TRUE!");
 				lip.val("false"); //IMPT: sending back the true value to controller to place it here
 
-				$(this).children(".row-1").children("i").addClass("icon-add_circle_outline-24px1");
-				$(this).children(".row-1").children("i").removeClass("icon-delete-24px");
+				$(this).children('div').first().children(".row-1").children("i").addClass("icon-add_circle_outline-24px1");
+				$(this).children('div').first().children(".row-1").children("i").removeClass("icon-delete-24px");
+				$(this).removeClass('location-selected')
 			}
 			else {
 				console.log("FALSE!");
 				lip.val("true"); //IMPT: sending back the true value to controller to place it here
 
-				$(this).children(".row-1").children("i").removeClass("icon-add_circle_outline-24px1");
-				$(this).children(".row-1").children("i").addClass("icon-delete-24px");
+				$(this).children('div').first().children(".row-1").children("i").removeClass("icon-add_circle_outline-24px1");
+				$(this).children('div').first().children(".row-1").children("i").addClass("icon-delete-24px");
+				$(this).addClass('location-selected')
 			}
 		}
 	});
