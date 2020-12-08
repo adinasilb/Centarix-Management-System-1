35b1d4bf1ffd801b1b1b3062fbd9b869d10e7e92
Adina Silberberg <adinasilberberg@gmail.com>
2020-12-08 10:05:42 +0200

    can x out contacts

---
+++ PrototypeWithAuth/wwwroot/js/vendorcontactinfopartial.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+	$(".vendor-contact-close").on("click", function () {
+		var contactcard = $(this).attr("ContactCardID");
+		console.log("closing contactcard: " + contactcard);
+		var contactcardid = "#contact-info-" + contactcard;
+		console.log("ContactCardID: " + contactcardid);
+		$(".contact-info").remove(contactcardid);
+		$('#contact-index').val(parseInt($('#contact-index').val()) - 1);
+	});
+});
