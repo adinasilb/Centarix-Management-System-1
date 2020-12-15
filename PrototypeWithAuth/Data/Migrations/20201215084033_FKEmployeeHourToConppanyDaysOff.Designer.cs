021b10c9efe50a81cfa5510422b535d9eba21194
DeborahShulman <shulman.deborah1@gmail.com>
2020-12-15 13:47:59 +0200

    fk employee hours to companydays off

--- PrototypeWithAuth/Data/Migrations/20201215084033_FKEmployeeHourToConppanyDaysOff.Designer.cs
+++ PrototypeWithAuth/Data/Migrations/20201215084033_FKEmployeeHourToConppanyDaysOff.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrototypeWithAuth.Data;
 
 namespace PrototypeWithAuth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201215084033_FKEmployeeHourToConppanyDaysOff")]
+    partial class FKEmployeeHourToConppanyDaysOff
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1386,6 +1388,11 @@ namespace PrototypeWithAuth.Data.Migrations
                         {
                             OffDayTypeID = 2,
                             Description = " VacationDay"
+                        },
+                        new
+                        {
+                            OffDayTypeID = 3,
+                            Description = "Company Day Off"
                         });
                 });
 
