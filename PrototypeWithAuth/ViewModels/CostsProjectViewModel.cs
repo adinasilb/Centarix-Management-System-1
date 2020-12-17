2b85700cf6529a9d808620f5decc1e6ec9a6f09a
DeborahShulman <shulman.deborah1@gmail.com>
2020-12-17 10:39:54 +0200

    cost projects view model

---
+++ PrototypeWithAuth/ViewModels/CostsProjectViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PrototypeWithAuth.Models;
+
+namespace PrototypeWithAuth.ViewModels
+{
+    public class CostsProjectViewModel
+    {
+        public IEnumerable<ParentCategory> ParentCategoryList { get; set; }
+        public int SelectedParentCategory { get; set; }
+        public IEnumerable<ProductSubcategory> SubCategoryList { get; set; }
+        public int SelectedProductSubCategory { get; set; }
+        public IEnumerable<Vendor> VendorList {get; set;}
+        public int SelectedVendor { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string InvoiceNumber { get; set; }
+    }
+}
