4c1e9ff86db70a95945ae817218bd6c612b0d85b
DeborahShulman <shulman.deborah1@gmail.com>
2020-12-23 11:31:35 +0200

    fixed timekeeper validation

--- PrototypeWithAuth/wwwroot/js/Validation/UpdateHoursValidation.js
+++ PrototypeWithAuth/wwwroot/js/Validation/UpdateHoursValidation.js
@@ -2,13 +2,22 @@
 $.validator.addMethod("eitherHoursOrTimeAndTimeRangeMakesSenseEntry1", function (value, element) {
 	return ($("#EmployeeHour_Exit1").val() != "" && $("#EmployeeHour_Entry1").val() != "") && ($('#EmployeeHour_Exit1').val() > $('#EmployeeHour_Entry1').val()) || $("#EmployeeHour_TotalHours").val() != "";
 }, 'Either total hours or Entry1 and Entry 2 must be filled in. Entry Must Be Less Than Exit');
-$.validator.addMethod("eitherHoursOrTimeAndTimeRangeMakesSenseEntry2", function (value, element) {
-	return ((($("#EmployeeHour_Exit1").val() != "" && $("#EmployeeHour_Entry1").val() != "") && ($('#EmployeeHour_Exit2').val() > $('#EmployeeHour_Entry2').val())) || $("#EmployeeHour_TotalHours").val() != "");
-}, 'Either total hours or Entry1 and Entry 2 must be filled in. Entry Must Be Less Than Exit');
+
+$.validator.addMethod("TimeRangeMakesSenseEntry2", function (value, element) {
+	return $('#EmployeeHour_Exit2').val() > $('#EmployeeHour_Entry2').val();
+}, 'Exit must be more than Entry');
 
 $.validator.addMethod("eitherHoursOrTime", function (value, element) {
 return ($("#EmployeeHour_Exit1").val() != "" && $("#EmployeeHour_Entry1").val() != "") || $("#EmployeeHour_TotalHours").val() != "";
 }, 'Either total hours or Entry1 and Entry 2 must be filled in');
+
+$.validator.addMethod("IfEntry2IsInView", function (value, element) {
+	return $('#EmployeeHour_Exit2').val()!="" || $('#EmployeeHour_Entry2').val() !="";
+}, 'Remove or fill out Entry and Exit 2');
+
+$.validator.addMethod("Entry2NotGreaterThanExitTwo", function (value, element) {
+	return $('#EmployeeHour_Entry2').val() > $('#EmployeeHour_Exit1').val();
+}, 'Entry 2 must be more than Exit 2');
 
 
 $('.UpdateHoursForm, .reportHoursForm').validate({
@@ -20,10 +29,15 @@ $('.UpdateHoursForm, .reportHoursForm').validate({
 			eitherHoursOrTimeAndTimeRangeMakesSenseEntry1: true
 		},
 		"EmployeeHour.Entry2": {
-			eitherHoursOrTimeAndTimeRangeMakesSenseEntry2: true
+			IfEntry2IsInView: true,
+			Entry2NotGreaterThanExitTwo: true,	
+			TimeRangeMakesSenseEntry2: true
+		
 		},
 		"EmployeeHour.Exit2": {
-			eitherHoursOrTimeAndTimeRangeMakesSenseEntry2: true
+			IfEntry2IsInView: true,
+			TimeRangeMakesSenseEntry2: true
+		
 		},
 		"EmployeeHour.TotalHours": {
 			eitherHoursOrTime: true,
