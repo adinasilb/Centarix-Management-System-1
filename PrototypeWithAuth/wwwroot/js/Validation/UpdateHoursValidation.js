2a93cde934ad1c5bafac6f4a626b0bf216c2c9f6
rachelstrauss <rachelstrauss8@gmail.com>
2021-01-19 10:46:31 +0200

    change update hours input fields to text

--- PrototypeWithAuth/wwwroot/js/Validation/UpdateHoursValidation.js
+++ PrototypeWithAuth/wwwroot/js/Validation/UpdateHoursValidation.js
@@ -18,34 +18,57 @@ $.validator.addMethod("Entry2NotGreaterThanExitTwo", function (value, element) {
 	return $('#EmployeeHour_Entry2').val() > $('#EmployeeHour_Exit1').val();
 }, 'Entry 2 must be more than Exit 2');
 
+$.validator.addMethod("validTime", function (value, element) {
+	var t = value.split(':');
+	if (t[0].length == 1) {
+		value = "0" + value;
+	}
+	if (t[2] != null) {
+		$("#"+element.id).val(t[0] + ":" + t[1]);
+		console.log($("#" +element.id).val());
+    }
+	//console.log($("value" + value.length));
+	var result = value.length == 0 || (/^\d\d:\d\d$/.test(value) &&
+		t[0] >= 0 && t[0] < 24 &&
+		t[1] >= 0 && t[1] < 60);
+	return result;
+}, "Invalid time");
 
 $('.UpdateHoursForm').validate({
 	rules: {
 		"EmployeeHour.Entry1": {
-			eitherHoursOrTimeAndTimeRangeMakesSenseEntry1: true
+			eitherHoursOrTimeAndTimeRangeMakesSenseEntry1: true,
+			validTime: true
 		},
 		"EmployeeHour.Exit1": {
-			eitherHoursOrTimeAndTimeRangeMakesSenseEntry1: true
+			eitherHoursOrTimeAndTimeRangeMakesSenseEntry1: true,
+			validTime: true
 		},
 		"EmployeeHour.Entry2": {
 			IfEntry2IsInView: true,
 			Entry2NotGreaterThanExitTwo: true,	
-			TimeRangeMakesSenseEntry2: true
+			TimeRangeMakesSenseEntry2: true,
+			validTime: true
 		},
 		"EmployeeHour.Exit2": {
 			IfEntry2IsInView: true,
-			TimeRangeMakesSenseEntry2: true
+			TimeRangeMakesSenseEntry2: true,
+			validTime: true
 		
 		},
 		"EmployeeHour.TotalHours": {
 			eitherHoursOrTime: true,
+			validTime: true
 		},
 		"EmployeeHour.EmployeeHoursStatusEntry1ID": {
 			required: true,
 		},
 		"EmployeeHour.EmployeeHoursStatusEntry2ID": {
 			required: true,
-		}
+		},
+		"EmployeeHour.PartialOffDayHours": {
+			validTime: true
+        }
 
 
 	}
