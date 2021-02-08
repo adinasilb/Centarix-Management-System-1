af6b98ee0ed7d59bbf44ae7762a915f2ebf5360b
DeborahShulman <shulman.deborah1@gmail.com>
2021-02-08 09:24:38 +0200

    LabPart Migration

---
+++ PrototypeWithAuth/Data/Migrations/20210208072353_AddedLabPartToDB.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrototypeWithAuth.Data.Migrations
+{
+    public partial class AddedLabPartToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LabParts",
+                columns: table => new
+                {
+                    LabPartID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LabPartName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LabParts", x => x.LabPartID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LabParts");
+        }
+    }
+}
