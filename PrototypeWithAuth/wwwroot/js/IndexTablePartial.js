977de51821f18c095926de528345367617034847
DeborahShulman <shulman.deborah1@gmail.com>
2021-02-23 13:32:16 +0200

    received modal shelves smaller

--- PrototypeWithAuth/wwwroot/js/IndexTablePartial.js
+++ PrototypeWithAuth/wwwroot/js/IndexTablePartial.js
@@ -52,7 +52,6 @@ $(".load-order-details").off('click').on("click", function (e) {
  
 $(".load-product-details").off('click').on("click", function (e) {
     
-    $(".load-product-details").off('click')
     e.preventDefault();
     e.stopPropagation();
     $("#loading").show();
@@ -72,7 +71,7 @@ $(".load-product-details").off('click').on("click", function (e) {
 //$("body, .modal").off('click', ".load-product-details-summary").on("click", ".load-product-details-summary", function (e) {
      
 $(".load-product-details-summary").off('click').on("click", function (e) {
-    $(".load-product-details-summary").off('click')
+ 
     e.preventDefault();
     e.stopPropagation();
     $("#loading").show();
