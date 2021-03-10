34deeba083174e91937db1ee514b45073174b34b
afinkel2 <adinaaidel59@gmail.com>
2021-03-10 14:55:22 +0200

    fixed val for add invoice

--- PrototypeWithAuth/wwwroot/js/Validation/AddInvoiceFormValidation.js
+++ PrototypeWithAuth/wwwroot/js/Validation/AddInvoiceFormValidation.js
@@ -1,6 +1,9 @@
 ﻿$('.addInvoiceForm').validate({
 	rules: {
-		"InvoiceImage": { required: true, extension: "jpg|jpeg|png|pdf" },
+		"InvoiceImage": {
+			required: true,
+			extension: "jpg|jpeg|png|pdf"
+		},
 		"Invoice.InvoiceNumber": {
 			required: true,
 			number: true,
