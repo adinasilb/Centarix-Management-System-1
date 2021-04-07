628faa04d8d3b92ca58c4363b35a3a4dda019e51
DeborahShulman <shulman.deborah1@gmail.com>
2021-04-07 08:29:38 +0300

    applyfilter click

--- PrototypeWithAuth/wwwroot/js/InventoryFilterScript.js
+++ PrototypeWithAuth/wwwroot/js/InventoryFilterScript.js
@@ -6,37 +6,47 @@
 $('#invFilterPopover').on('shown.bs.popover', function () {
     $('body').addClass('popover-open');
 })
-$('.btn-filter').click(function () {
-        
+$('.btn-filter').click(function () {     
+	
+   var data = $.fn.BindSelectedFilters();
+   $.ajax({
+			processData: false,
+			contentType: false,
+			data: data,
+			async: true,
+			url: "/Requests/_InventoryFilterResults",
+			type: 'GET',
+			cache: false,
+			success: function (data) {
+
+            }
+    });
+
+});
+
+$("#applyFilter").click(function(){
+   var data = $.fn.BindSelectedFilters();
+    $.ajax({
+		processData: false,
+		contentType: false,
+		data: data,
+		async: true,
+		url: "/Requests/_IndexTableData?"+$.fn.getRequestIndexString(),
+		type: 'GET',
+		cache: false,
+		success: function (data) {
+			$("._IndexTableData").html(data);
+        }
+    });
+
+
+});
+
+$.fn.BindSelectedFilters = function(){
     var selectedVendor = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
     var selectedOwner = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
     var selectedLocation = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
     var selectedCategory = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
     var selectedSubCategory = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
-    $.ajax({
-        async: true,
-        url: "Requests/_InventoryFilterResults",
-        type: 'GET',
-        //cache: true,
-        success: function (data) {
-
-        }
-    });
-
-})
-
-
-
-
-
-
-
-
-
-$("#applyFilter").click(function(){
-
-
-
-
-
-});
+	return {SelectedCategories: selectedCategory, SelectedSubCategories : selectedSubCategory, SelectedLocations : selectedLocation, SelectedVendors: selectedVendor, SelectedOwner : selectedOwner}
+}
