b8440c9215400a2a4da6a63aadc7f22c2946e229
afinkel2 <adinaaidel59@gmail.com>
2021-04-07 08:14:18 +0300

    get all data in jquery

--- PrototypeWithAuth/wwwroot/js/InventoryFilterScript.js
+++ PrototypeWithAuth/wwwroot/js/InventoryFilterScript.js
@@ -6,3 +6,21 @@
 $('#invFilterPopover').on('shown.bs.popover', function () {
     $('body').addClass('popover-open');
 })
+$('.btn-filter').click(function () {
+        
+    var selectedVendor = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
+    var selectedOwner = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
+    var selectedLocation = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
+    var selectedCategory = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
+    var selectedSubCategory = $(".subcategory-col .selected button").map(function () { return $(this).attr("value"); }).get();
+    $.ajax({
+        async: true,
+        url: "Requests/_InventoryFilterResults",
+        type: 'GET',
+        //cache: true,
+        success: function (data) {
+
+        }
+    });
+
+})
