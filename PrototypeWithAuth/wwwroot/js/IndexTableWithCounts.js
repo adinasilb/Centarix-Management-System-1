8f25b64b7c0a2b640ceab628afe5ed7a04b86aba
afinkel2 <adinaaidel59@gmail.com>
2021-04-08 11:27:15 +0300

    clear filter method

--- PrototypeWithAuth/wwwroot/js/IndexTableWithCounts.js
+++ PrototypeWithAuth/wwwroot/js/IndexTableWithCounts.js
@@ -5,22 +5,12 @@
     $(this).addClass("active")
     //reset page number
     $('.page-number').val(1);
-    clearFilter();
+    $.fn.ClearFilter();
     ajaxPartialIndexTable($(this).attr("value"), "/Requests/_IndexTable", "._IndexTable", "GET")
 });
 
 
-function clearFilter() {
-    $.ajax({
-        async: true,
-        url: "/Requests/_InventoryFilterResults?selectedFilters=null&numFilters=0",
-        type: 'GET',
-        cache: false,
-        success: function(newData) {
-            $('#inventoryFilterContent').html(newData);
-        }
-    });
-}
+
 //function ajaxCallToPartialTable(status) {
 //    var selectedPriceSort = [];
 //    $("#priceSortContent .priceSort:checked").each(function (e) {
