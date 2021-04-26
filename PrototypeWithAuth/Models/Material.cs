739b9e945071b89bd8c66541dde555e7d9bcff18
DeborahShulman <shulman.deborah1@gmail.com>
2021-04-26 14:23:37 +0300

    deleted material category table

--- PrototypeWithAuth/Models/Material.cs
+++ PrototypeWithAuth/Models/Material.cs
@@ -14,7 +14,8 @@ namespace PrototypeWithAuth.Models
         public string Name { get; set; }
         public int ProductID { get; set; }
         public Product Product { get; set; }
-        public IEnumerable<MaterialProtocol> MaterialProtocols { get; set; }
+        public int ProtocolID { get; set; }
+        public Protocol Protocol { get; set; }
         public int MaterialCategoryID { get; set; }
         public MaterialCategory MaterialCategory { get; set; }
     }
