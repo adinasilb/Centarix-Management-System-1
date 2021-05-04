5a5a6bfea9bc87c8eb09bfd2e88b2b909f3486c0
afinkel2 <adinaaidel59@gmail.com>
2021-05-04 16:08:03 +0300

    added expected supply days to upload quote and order modals

--- PrototypeWithAuth/ViewModels/UploadOrderViewModel.cs
+++ PrototypeWithAuth/ViewModels/UploadOrderViewModel.cs
@@ -3,6 +3,7 @@ using PrototypeWithAuth.AppData.UtilityModels;
 using PrototypeWithAuth.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,6 +15,10 @@ namespace PrototypeWithAuth.ViewModels
         public List<string> FileStrings { get; set; }
         public AppUtility.MenuItems SectionType { get; set; }
         public RequestIndexObject RequestIndexObject { get; set;}
+        [Display(Name = "Expected Supply Days")]
 
+        [Range(0, 2147483647, ErrorMessage = "Field must be a positive number")]
+        public byte? ExpectedSupplyDays { get; set; }
+        public bool IsReorder { get; set; }
     }
 }
