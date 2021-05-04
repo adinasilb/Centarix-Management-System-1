cc1d1c30bdb52eb8a3204e45ea0598ff0b6f7f89
Adina Silberberg <adinasilberberg@gmail.com>
2021-05-04 12:44:10 +0300

    added favorite resources

--- PrototypeWithAuth/Data/ApplicationUser.cs
+++ PrototypeWithAuth/Data/ApplicationUser.cs
@@ -56,6 +56,7 @@ namespace PrototypeWithAuth.Data
         public IEnumerable<ShareRequest> ShareRequestsReceived { get; set; }
         public IEnumerable<FavoriteRequest> FavoriteRequests { get; set; }
         public IEnumerable<FavoriteProtocol> FavoriteProtocols { get; set; }
+        public IEnumerable<FavoriteResource> FavoriteResources { get; set; }
 
         // public string URLPic { get; set; }
 
---
+++ PrototypeWithAuth/Data/Migrations/20210504094309_addedFavoriteResources.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrototypeWithAuth.Data.Migrations
+{
+    public partial class addedFavoriteResources : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteResources",
+                columns: table => new
+                {
+                    FavoriteResourceID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProtocolID = table.Column<int>(nullable: false),
+                    ApplicationUserID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteResources", x => x.FavoriteResourceID);
+                    table.ForeignKey(
+                        name: "FK_FavoriteResources_AspNetUsers_ApplicationUserID",
+                        column: x => x.ApplicationUserID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteResources_ApplicationUserID",
+                table: "FavoriteResources",
+                column: "ApplicationUserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteResources");
+        }
+    }
+}
