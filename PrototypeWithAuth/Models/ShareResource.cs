8602463fde371f133e6ff54d991c5babc9a51911
Adina Silberberg <adinasilberberg@gmail.com>
2021-05-10 10:27:41 +0300

    added share resources

---
+++ PrototypeWithAuth/Models/ShareResource.cs
@@ -0,0 +1,32 @@
+﻿using PrototypeWithAuth.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototypeWithAuth.Models
+{
+    public class ShareResource
+    {
+        private DateTime _timestamp { get; set; }
+        [Key]
+        public int ShareResourceID { get; set; }
+        public int ResourceID { get; set; }
+        public Resource Resource { get; set; }
+        public string FromApplicationUserID { get; set; }
+        [ForeignKey("FromApplicationUserID")]
+        public ApplicationUser FromApplicationUser { get; set; }
+        public string ToApplicationUserID { get; set; }
+        [ForeignKey("ToApplicationUserID")]
+        public ApplicationUser ToApplicationUser { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp
+        {
+            get => _timestamp == new DateTime() ? DateTime.Now : _timestamp;
+            set { _timestamp = value; }
+        }
+    }
+}
