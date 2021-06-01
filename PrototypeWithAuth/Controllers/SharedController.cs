d5e875c16e9303a1906485fefdfc7e221d007083
Adina Silberberg <adinasilberberg@gmail.com>
2021-06-01 07:59:33 +0300

    fixed bugs, confirm email modal works

--- PrototypeWithAuth/Controllers/SharedController.cs
+++ PrototypeWithAuth/Controllers/SharedController.cs
@@ -2285,7 +2285,7 @@ namespace PrototypeWithAuth.Controllers
                             }
                             await _context.SaveChangesAsync();
                             await transaction.CommitAsync();
-                            return new RedirectToActionResult("Index", controller, termsViewModel.RequestIndexObject);
+                            return new RedirectAndModel() { RedirectToActionResult = new RedirectToActionResult("Index", controller, termsViewModel.RequestIndexObject) };
                         }
 
                     }
@@ -2296,7 +2296,7 @@ namespace PrototypeWithAuth.Controllers
                     }
 
                 }
-                return RedirectToAction("ConfirmEmailModal", controller, termsViewModel.RequestIndexObject);
+                return new RedirectAndModel() {RedirectToActionResult = new RedirectToActionResult("ConfirmEmailModal", controller, termsViewModel.RequestIndexObject) };
             }
             catch (Exception ex)
             {
