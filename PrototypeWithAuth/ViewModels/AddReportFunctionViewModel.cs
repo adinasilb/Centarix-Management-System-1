22bd2dd80c23de0d9687494b71ae8dad9b6ffec2
rachelstrauss <rachelstrauss8@gmail.com>
2021-06-07 10:45:25 +0300

    File function and report number

---
+++ PrototypeWithAuth/ViewModels/AddReportFunctionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PrototypeWithAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototypeWithAuth.ViewModels
+{
+    public class AddReportFunctionViewModel : AddFunctionViewModel
+    {
+        public FunctionType FunctionType { get; set; }
+        public int ReportID { get; set; }
+    }
+}
