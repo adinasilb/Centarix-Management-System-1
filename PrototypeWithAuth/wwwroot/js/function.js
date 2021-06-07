51188dc97536e3435669c49c6f496bbe85124c9d
rachelstrauss <rachelstrauss8@gmail.com>
2021-06-07 11:32:10 +0300

    move function scripts and report text partial view

--- PrototypeWithAuth/wwwroot/js/function.js
+++ PrototypeWithAuth/wwwroot/js/function.js
@@ -9,3 +9,46 @@
 	}
 	$.fn.CallPageRequest( url , "addFunction");
 });
+
+$(".saveFunction").click(function (e) {
+    e.preventDefault();
+
+    //$('.materialForm').data("validator").settings.ignore = "";
+    //var valid = $('.materialForm').valid();
+    //console.log("valid form: " + valid)
+    //if (!valid) {
+    //    e.preventDefault();
+    //    if (!$('.activeSubmit').hasClass('disabled-submit')) {
+    //        $('.activeSubmit').addClass('disabled-submit')
+    //    }
+
+    //}
+    //else {
+    //$('.activeSubmit').removeClass('disabled-submit')
+    var formData = new FormData($(".addFunctionForm")[0]);
+    $.ajax({
+        url: "/Protocols/AddFunctionModal",
+        traditional: true,
+        data: formData,
+        contentType: false,
+        processData: false,
+        type: "POST",
+        success: function (data) {
+            if ($("#masterPageType").val() == "ProtocolsReports") {
+                $(".report-text").html(data);
+            }
+            else {
+                $("._Lines").html(data);
+            }
+            $.fn.CloseModal('add-function');
+        },
+        error: function (jqxhr) {
+            if (jqxhr.status == 500) {
+                $.fn.OpenModal('modal', 'add-function', jqxhr.responseText);
+            }
+            return true;
+        }
+    });
+    //}
+    //$('.materialForm').data("validator").settings.ignore = ':not(select:hidden, input:visible, textarea:visible)';
+});
