47eddb98e285ac40fd329c675a2012063a04cf25
Adina Silberberg <adinasilberberg@gmail.com>
2021-06-16 09:01:42 +0300

    added personal resources

---
+++ PrototypeWithAuth/Data/Migrations/20210616054839_addedPersonalToResources.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrototypeWithAuth.Data.Migrations
+{
+    public partial class addedPersonalToResources : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserCreatorID",
+                table: "Resources",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPersonal",
+                table: "Resources",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_ApplicationUserCreatorID",
+                table: "Resources",
+                column: "ApplicationUserCreatorID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resources_AspNetUsers_ApplicationUserCreatorID",
+                table: "Resources",
+                column: "ApplicationUserCreatorID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resources_AspNetUsers_ApplicationUserCreatorID",
+                table: "Resources");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Resources_ApplicationUserCreatorID",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserCreatorID",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "IsPersonal",
+                table: "Resources");
+        }
+    }
+}
