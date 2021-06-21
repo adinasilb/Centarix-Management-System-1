9988d8a53248b34e12ce98a88378c5fcf2ef908d
afinkel2 <adinaaidel59@gmail.com>
2021-06-21 10:35:44 +0300

    expected suppply date required and min 0

--- PrototypeWithAuth/wwwroot/js/Validation/EditQuoteDetailsFormValidation.js
+++ PrototypeWithAuth/wwwroot/js/Validation/EditQuoteDetailsFormValidation.js
@@ -4,17 +4,24 @@ $('.editQuoteDetails').validate({
     return $.trim( value );
   },
 	rules: {
-		"Request.ParentQuote.QuoteNumber": {
+		"QuoteNumber": {
 			required: true,
 			number: true,
 			min: 1
 		},
-		"Request.ExpectedSupplyDays": {
-			required: true,
-			min: 0,
-			integer: true
-		},
+		//"Request.ExpectedSupplyDays": {
+		//	required: true,
+		//	min: 0,
+		//	integer: true
+		//},
 		"QuoteFileUpload": { required: true, extension: "jpg|jpeg|png|pdf" }
 	},
 
+});
+$(".expected-supply-days").each(function () {
+	$(this).rules("add", {
+		required: true,
+		min: 0,
+		integer: true
+	});
 });
