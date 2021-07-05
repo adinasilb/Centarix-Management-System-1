c34a1de9a681cd73470455b2a0159b2b34ff5b4a
afinkel2 <adinaaidel59@gmail.com>
2021-07-05 12:31:16 +0300

    calc expected supply date individually in edit quote details

--- PrototypeWithAuth/wwwroot/js/WarantySupplyDateCalc.js
+++ PrototypeWithAuth/wwwroot/js/WarantySupplyDateCalc.js
@@ -1,42 +1,54 @@
 ﻿
-	$(".expected-supply-days").change(function () {
-		var OrderDate; 
-		if ($('.for-supply-date-calc').length > 0) {
-			OrderDate =moment($('.for-supply-date-calc').val()/*.split("/").reverse().join("-")*/);
-			
-		} else {
-			OrderDate = moment();
-			
-		}
-		var supplyDate = OrderDate.add($(this).val(), "d").format("D MMM YYYY");
-		console.log(supplyDate)
-		$("input[name='expected-supply-days']").val(supplyDate);
-		$("input[name='expected-supply-days']").attr("data-val",supplyDate/*.split("/").reverse().join("-")*/);
+$(".expected-supply-days").change(function () {
+	var selector = "input[name='expected-supply-days']";
+	if ($(this).attr("index") != null) {
+		selector += "." + $(this).attr("index");
+	}
+	//console.log(this)
+	$.fn.SetExpectedSupplyDateFromDays(selector, $(this));
 
-	});
+});
+$.fn.SetExpectedSupplyDateFromDays = function (selector, thisElement) {
+	var OrderDate;
+/*	if ($('.for-supply-date-calc').length > 0) {
+		OrderDate = moment($('.for-supply-date-calc').val()*//*.split("/").reverse().join("-")*//*);
 
+	} else {*/
+		OrderDate = moment();
+//	}
+	var supplyDate = OrderDate.add(thisElement.val(), "d").format("D MMM YYYY");
+	console.log(supplyDate)
+	$(selector).val(supplyDate);
+	$(selector).attr("data-val", supplyDate/*.split("/").reverse().join("-")*/);
+}
 
-	$("#expected-supply-date").change(function () {
-		var val =$(this).val();
-		val = val.split("/").reverse().join("-");
-		var date =moment(val);
-		date.set("time", 0)
-		if (date < moment() && !moment().isSame(date, 'day')) {
-			return;
-		}
-		var OrderDate; 
-		if ($('.for-supply-date-calc').length > 0) {
-			console.log("in first of of check roder date")
-			OrderDate =moment($('.for-supply-date-calc').val().split("/").reverse().join("-"));
-			
-		} else {
-			OrderDate = moment();
-			
-		}
-		var amountOfDays = Math.ceil(date.diff(OrderDate,'days', true))
-		$(".expected-supply-days").val(amountOfDays);
-	});
+$("#expected-supply-date").change(function () {
+	var selector = ".expected-supply-days";
+	if ($(this).attr("index") != null) {
+		selector += "." + $(this).attr("index");
+	}
+	$.fn.SetExpectedSupplyDaysFromDate(selector, $(this));
+});
+$.fn.SetExpectedSupplyDaysFromDate = function(selector, thisElement) {
+	var val = thisElement.val();
+	val = val.split("/").reverse().join("-");
+	var date = moment(val);
+	date.set("time", 0)
+	if (date < moment() && !moment().isSame(date, 'day')) {
+		return;
+	}
+	var OrderDate;
+	//if ($('.for-supply-date-calc').length > 0) {
+	//	console.log("in first of of check roder date")
+	//	OrderDate = moment($('.for-supply-date-calc').val().split("/").reverse().join("-"));
 
+	//} else {
+		OrderDate = moment();
+
+	//}
+	var amountOfDays = Math.ceil(date.diff(OrderDate, 'days', true))
+	$(selector).val(amountOfDays);
+}
 $(".warranty-months").change(function () {
 			var date; 
 		if ($("#Request_ParentRequest_OrderDate").length > 0) {
