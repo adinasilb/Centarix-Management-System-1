5083279053dce94676f3867d5e6649b408f3e72d
Adina Silberberg <adinasilberberg@gmail.com>
2021-07-12 13:59:18 +0300

    fixed temprequestjson,added shipping boolean

---
+++ PrototypeWithAuth/Data/Migrations/20210712105602_addedIsShippingPaid.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrototypeWithAuth.Data.Migrations
+{
+    public partial class addedIsShippingPaid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsShippingPaid",
+                table: "ParentRequests",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsShippingPaid",
+                table: "ParentRequests");
+        }
+    }
+}
