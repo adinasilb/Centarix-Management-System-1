a67d6f4caa24185a2b16110b910ad6b2cf1384f3
DeborahShulman <shulman.deborah1@gmail.com>
2021-07-27 16:25:17 +0300

    startup

--- PrototypeWithAuth/Startup.cs
+++ PrototypeWithAuth/Startup.cs
@@ -72,7 +72,7 @@ namespace PrototypeWithAuth
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DevelpersDB"));
+                    Configuration.GetConnectionString("DevelopersDB"));
                 options.EnableSensitiveDataLogging(true);
             });
 
