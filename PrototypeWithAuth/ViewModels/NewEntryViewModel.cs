fe3b7f017957acbc4a66269bd3a659e1a5d23481
Adina Silberberg <adinasilberberg@gmail.com>
2021-08-03 11:19:11 +0300

    started new entry

---
+++ PrototypeWithAuth/ViewModels/NewEntryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PrototypeWithAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototypeWithAuth.ViewModels
+{
+    public class NewEntryViewModel
+    {
+        public DateTime DateTime { get; set; }
+        public int VisitNumber { get; set; }
+        public int SiteID { get; set; }
+        public IEnumerable<Site> Sites { get; set; }
+    }
+}
