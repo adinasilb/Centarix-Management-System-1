5620d940efb1bc8fde0427285145d7431ce7ef08
afinkel2 <adinaaidel59@gmail.com>
2021-08-18 11:42:07 +0300

    reusable enum fix

--- PrototypeWithAuth/AppData/AppUtility.cs
+++ PrototypeWithAuth/AppData/AppUtility.cs
@@ -148,7 +148,7 @@ namespace PrototypeWithAuth.AppData
         public enum PaymentsEnum { ToPay, PayNow }
         public enum SuppliersEnum { All, NewSupplier, Search }
         public enum CategoryTypeEnum { Operations, Lab }
-        public enum ParentCategoryEnum { Consumables, ReagentsAndChemicals, Samples, Reusables, Equipment, Operation, Biological, Safety, General, Clinical }
+        public enum ParentCategoryEnum { Consumables, ReagentsAndChemicals, Samples, Reusable, Equipment, Operation, Biological, Safety, General, Clinical }
         public enum RequestModalType { Create, Edit, Summary }
         public enum ProtocolModalType { Create, CheckListMode, Summary, Edit, SummaryFloat }
         public enum OrderTypeEnum { RequestPriceQuote, OrderNow, AddToCart, AskForPermission, AlreadyPurchased, Save, SaveOperations, ExcelUpload }
