d06cc519264a3604c6c0aef28c19ada9aa90ad1b
afinkel2 <adinaaidel59@gmail.com>
2021-09-13 17:03:25 +0300

    appsettings

--- PrototypeWithAuth/Startup.cs
+++ PrototypeWithAuth/Startup.cs
@@ -73,7 +73,7 @@ namespace PrototypeWithAuth
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection"));
+                    Configuration.GetConnectionString("DevelopersDB"));
                 options.EnableSensitiveDataLogging(true);
             });
 
