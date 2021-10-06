0506b252ad439425c8e2e38e46123913f9909bc9
rachelstrauss <rachelstrauss8@gmail.com>
2021-10-06 12:50:09 +0300

    add location type and room instances

---
+++ DeleteRequestsProducts2.sql
@@ -0,0 +1,30 @@
+﻿delete from RequestNotifications 
+go
+delete from Comments
+go
+delete from RequestLocationInstances
+go
+update LocationInstances set IsFull = 0
+go
+delete from Payments
+go
+delete from Materials
+go
+delete from FunctionLines
+go
+delete from FavoriteRequests
+go
+delete from ShareRequests
+go
+delete from Requests
+go 
+delete from ParentRequests
+go
+delete from ParentQuotes
+go
+
+delete  from products
+go
+delete from Invoices
+go
+
