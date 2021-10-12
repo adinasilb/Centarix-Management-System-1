5bc95c50f92315dde6dc5870b1eb66dd83b074d0
DeborahShulman <shulman.deborah1@gmail.com>
2021-10-12 10:15:36 +0300

    update settins

--- PrototypeWithAuth/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ PrototypeWithAuth/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -189,7 +189,7 @@ namespace PrototypeWithAuth.Areas.Identity.Pages.Account
         {
             return string.Format(
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("ElixirTest"),
+                _urlEncoder.Encode("Elixir"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
