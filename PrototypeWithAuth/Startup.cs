bc385b21da1d7d92c197a4c2cc8d2d808a61797f
DeborahShulman <shulman.deborah1@gmail.com>
2021-10-20 10:18:06 +0300

    files

--- PrototypeWithAuth/Startup.cs
+++ PrototypeWithAuth/Startup.cs
@@ -73,7 +73,7 @@ namespace PrototypeWithAuth
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("ElixirTestUpload"));
+                    Configuration.GetConnectionString("ElixirTest"));
                 options.EnableSensitiveDataLogging(true);
             });
 
