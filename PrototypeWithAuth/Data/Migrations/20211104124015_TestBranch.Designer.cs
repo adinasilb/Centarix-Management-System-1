19b4e12e6e0ba66d16688ae80978a3cedb0220c4
Adina Silberberg <adinasilberberg@gmail.com>
2021-11-04 15:12:32 +0200

    unique index on participant centarixid

--- PrototypeWithAuth/Data/Migrations/20211104124015_TestBranch.Designer.cs
+++ PrototypeWithAuth/Data/Migrations/20211104124015_TestBranch.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrototypeWithAuth.Data;
 
 namespace PrototypeWithAuth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211104124015_TestBranch")]
+    partial class TestBranch
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -3363,7 +3365,7 @@ namespace PrototypeWithAuth.Data.Migrations
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
                     b.Property<string>("CentarixID")
-                        .HasColumnType("nvarchar(450)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("DOB")
                         .HasColumnType("datetime2");
@@ -3384,10 +3386,6 @@ namespace PrototypeWithAuth.Data.Migrations
                     b.HasIndex("GenderID");
 
                     b.HasIndex("ParticipantStatusID");
-
-                    b.HasIndex("ParticipantID", "CentarixID")
-                        .IsUnique()
-                        .HasFilter("[CentarixID] IS NOT NULL");
 
                     b.ToTable("Participants");
                 });
---
+++ PrototypeWithAuth/Data/Migrations/20211104130630_addedParticipantIDUniqueValidation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrototypeWithAuth.Data.Migrations
+{
+    public partial class addedParticipantIDUniqueValidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_ParticipantID_ParticipantStatusID",
+                table: "Participants",
+                columns: new[] { "ParticipantID", "ParticipantStatusID" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Participants_ParticipantID_ParticipantStatusID",
+                table: "Participants");
+        }
+    }
+}
