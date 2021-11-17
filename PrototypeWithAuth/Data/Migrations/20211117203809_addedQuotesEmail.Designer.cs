12ece010e0eca3a62eba028694c3d5bd24988d98
Adina Silberberg <adinasilberberg@gmail.com>
2021-11-17 23:05:25 +0200

    added quotesemail to model

--- PrototypeWithAuth/Data/Migrations/20211117203809_addedQuotesEmail.Designer.cs
+++ PrototypeWithAuth/Data/Migrations/20211117203809_addedQuotesEmail.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrototypeWithAuth.Data;
 
 namespace PrototypeWithAuth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211117203809_addedQuotesEmail")]
+    partial class addedQuotesEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -6795,7 +6797,6 @@ namespace PrototypeWithAuth.Data.Migrations
                         .HasMaxLength(50);
 
                     b.Property<string>("QuotesEmail")
-                        .IsRequired()
                         .HasColumnType("nvarchar(50)")
                         .HasMaxLength(50);
 
