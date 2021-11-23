de52f394ab89b3a40efd69cbc4c34c1ef9851faa
DeborahShulman <shulman.deborah1@gmail.com>
2021-11-23 14:59:16 +0200

    modelredos

--- PrototypeWithAuth/Models/ProtocolComment.cs
+++ PrototypeWithAuth/Models/ProtocolComment.cs
@@ -4,25 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using PrototypeWithAuth.Data;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PrototypeWithAuth.Models
 {
-    public class ProtocolComment
+    public class ProtocolComment : CommentBase
     {
-        [Key]
-        public int ProtocolCommentID { get; set; }
-        public string ProtocolCommmentType { get; set; }
-        public string ProtocolCommentDescription { get; set; }
-        public string ApplicationUserCreatorID { get; set; }
-        public ApplicationUser ApplicationUserCreator { get; set; }
 
-
-        private DateTime _CommentTimeStamp;
-        [DataType(DataType.Date)]
-        public DateTime CommentTimeStamp
-        {
-            get => _CommentTimeStamp == new DateTime() ? DateTime.Now : _CommentTimeStamp;
-            set { _CommentTimeStamp = value; }
-        }
+        [ForeignKey("ObjectID")]
+        public Protocol Protocol { get; set; }
     }
 }
