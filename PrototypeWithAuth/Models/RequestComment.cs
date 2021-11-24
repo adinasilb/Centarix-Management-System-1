b483cac0f1ab53e44b21dd3daff0d0b41177e883
DeborahShulman <shulman.deborah1@gmail.com>
2021-11-24 14:01:44 +0200

    fixedu p all the comment changeover

--- PrototypeWithAuth/Models/RequestComment.cs
+++ PrototypeWithAuth/Models/RequestComment.cs
@@ -5,15 +5,20 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using PrototypeWithAuth.Data;
 using System.ComponentModel.DataAnnotations.Schema;
+using PrototypeWithAuth.AppData;
 
 namespace PrototypeWithAuth.Models
 {
-    public class RequestComment:CommentBase
+    public class RequestComment : CommentBase
     {
 
         [ForeignKey("ObjectID")]
         public Request Request { get; set; }
-       
+        public override AppUtility.CommentModelTypeEnum ModelType
+        {
+            get { return AppUtility.CommentModelTypeEnum.Request; }
+
+        }
     }
 }
 
