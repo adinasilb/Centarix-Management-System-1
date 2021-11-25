eede7880eea6aa7c43d7fc454df96eaf76cfbb1e
DeborahShulman <shulman.deborah1@gmail.com>
2021-11-25 15:16:28 +0200

    add warning to product

--- PrototypeWithAuth/Models/CommentBase.cs
+++ PrototypeWithAuth/Models/CommentBase.cs
@@ -31,6 +31,5 @@ namespace PrototypeWithAuth.Models
         public CommentType CommentType { get; set; }
         public bool IsDeleted { get; set; }
         public int ObjectID { get; set; }
-        public abstract AppUtility.CommentModelTypeEnum ModelType { get; }
     }
 }
