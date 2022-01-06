8ec3fb5e9cb937cba3aee0cf35da43591c741372
DeborahShulman <shulman.deborah1@gmail.com>
2022-01-06 13:42:56 +0200

    fixed startup

--- PrototypeWithAuth/Startup.cs
+++ PrototypeWithAuth/Startup.cs
@@ -74,7 +74,7 @@ namespace PrototypeWithAuth
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("ElixirCopy"), sqlServerOptions => sqlServerOptions.CommandTimeout(120));
+                    Configuration.GetConnectionString("DeveloperDB"), sqlServerOptions => sqlServerOptions.CommandTimeout(120));
                     options.EnableSensitiveDataLogging(true);   
             });
 
