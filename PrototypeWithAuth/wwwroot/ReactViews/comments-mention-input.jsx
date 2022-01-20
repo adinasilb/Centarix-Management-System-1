bf8626e25df0e6eb38fd48813af3f3d0e47c809a
DeborahShulman <shulman.deborah1@gmail.com>
2022-01-20 15:10:38 +0200

    more config

---
+++ PrototypeWithAuth/wwwroot/ReactViews/comments-mention-input.jsx
@@ -0,0 +1,38 @@
+﻿import React from 'react';
+import { MentionsInput, Mention } from 'react-mentions'
+
+export default class CommentInput extends React.Component {
+
+    constructor(props) {
+        // Required step: always call the parent class' constructor
+        super(props);
+
+        // Set the state directly. Use props if necessary.
+        this.state = {
+           value:""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.renderUserSuggestion = this.renderUserSuggestion.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    renderUserSuggestion() {
+        console.log("rendersuggestion")
+    }
+
+    render() {
+        return (
+            <MentionsInput value={this.state.value} onChange={this.handleChange}>
+                <Mention
+                    trigger="@"
+                    data={this.props.users}
+                    renderSuggestion={this.renderUserSuggestion}
+                />
+            </MentionsInput>
+        );
+    }
+}
