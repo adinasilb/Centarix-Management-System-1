a0bedafc09e348631dc820ba1c18b34dfff612ff
DeborahShulman <shulman.deborah1@gmail.com>
2022-01-20 10:26:37 +0200

    configure react

---
+++ PrototypeWithAuth/wwwroot/ReactViews/expose-components.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
+
+
+// any css-in-js or other libraries you want to use server-side
+import ActionBar from './action-bar.jsx';
+
+global.React = React;
+global.ReactDOM = ReactDOM;
+global.ReactDOMServer = ReactDOMServer;
+
+global.ActionBar =  ActionBar ;
