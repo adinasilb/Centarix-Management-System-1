d3472af126b2df588203c23b5e39f895231ebe21
DeborahShulman <shulman.deborah1@gmail.com>
2022-01-20 15:10:38 +0200

    more config

---
+++ PrototypeWithAuth/wwwroot/ReactViews/expose-components.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
+
+
+// any css-in-js or other libraries you want to use server-side
+import CommentInput from './comments-mention-input.jsx';
+
+global.React = React;
+global.ReactDOM = ReactDOM;
+global.ReactDOMServer = ReactDOMServer;
+
+global.CommentInput = CommentInput ;
