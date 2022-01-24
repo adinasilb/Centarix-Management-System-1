86fafd52c73ea9fbee6e7c14141266b98cd35818
DeborahShulman <shulman.deborah1@gmail.com>
2022-01-24 13:54:11 +0200

    removed comment mentions code

--- PrototypeWithAuth/wwwroot/ReactViews/expose-components.js
+++ PrototypeWithAuth/wwwroot/ReactViews/expose-components.js
@@ -2,13 +2,8 @@
 import ReactDOM from 'react-dom';
 import ReactDOMServer from 'react-dom/server';
 
-
-// any css-in-js or other libraries you want to use server-side
-import CommentInput from './comments-mention-input.jsx';
-
 global.React = React;
 global.ReactDOM = ReactDOM;
 global.ReactDOMServer = ReactDOMServer;
 
-global.CommentInput = CommentInput;
 
