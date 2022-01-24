a1e66c9b62e3e288a977e447284be4c414fd1688
DeborahShulman <shulman.deborah1@gmail.com>
2022-01-24 13:56:16 +0200

    Revert "Revert "removed comment mentions code""
    This reverts commit 99463a8d657c835219602e2c3ddb79cc8802eb37.

--- PrototypeWithAuth/ViewModels/CommentsInfoViewModel.cs
+++ PrototypeWithAuth/ViewModels/CommentsInfoViewModel.cs
@@ -13,7 +13,5 @@ namespace PrototypeWithAuth.ViewModels
         public CommentBase Comment { get; set; }
         public bool IsEdit { get; set; }
         public bool IsRemove { get; set; }
-        public List<MentionsModel> MentionUsers { get; set; }
-        public List<MentionsModel> MentionItems { get; set; }
     }
 }
