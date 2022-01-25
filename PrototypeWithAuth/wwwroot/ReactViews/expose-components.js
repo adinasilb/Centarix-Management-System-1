fba3b19904410a162cfe4a11042dc8adc517c8a6
DeborahShulman <shulman.deborah1@gmail.com>
2022-01-25 09:08:33 +0200

    floating action bar start

--- PrototypeWithAuth/wwwroot/ReactViews/expose-components.js
+++ PrototypeWithAuth/wwwroot/ReactViews/expose-components.js
@@ -2,8 +2,12 @@
 import ReactDOM from 'react-dom';
 import ReactDOMServer from 'react-dom/server';
 
+import FloatingActionBar from './floating-action-bar.jsx'
+
 global.React = React;
 global.ReactDOM = ReactDOM;
 global.ReactDOMServer = ReactDOMServer;
+
+global.FloatingActionBar = FloatingActionBar;
 
 
