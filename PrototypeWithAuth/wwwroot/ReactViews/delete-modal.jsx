b5869ad4277a4bef6b98c6ceaefc7c8317fc067d
DeborahShulman <shulman.deborah1@gmail.com>
2022-01-25 23:15:59 +0200

    started configureing the deltee modal to the delete button

---
+++ PrototypeWithAuth/wwwroot/ReactViews/delete-modal.jsx
@@ -0,0 +1,32 @@
+﻿import React, { Component } from 'react';
+import createPortal  from 'react-dom';
+export default class DeleteModal extends Component {
+    state = { isLoaded: false, view: "" };
+
+    componentDidMount() {
+        fetch("/Requests/DeleteModal?id=4", {
+            method: "GET"            
+        })
+           
+            .then(res => {
+                this.setState({
+                    view: res,
+                    isLoaded: true
+                });
+            });
+    }
+
+    render() {
+        const { isLoaded } = this.state;
+
+        if (!isLoaded) {
+            return <div>Loading...</div>;
+        }
+        return createPortal(this.state.view,
+        document.getElementById("deleteItem")
+        );
+    }
+}
+//if (typeof window !== 'undefined') {
+//    ReactDOM.render(<DeleteModal />, document.getElementById("deleteItem"));
+//}
