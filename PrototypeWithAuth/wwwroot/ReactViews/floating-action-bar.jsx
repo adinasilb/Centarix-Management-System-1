282bd159c327c822b9a7a241ccd26bb3eeda74e2
DeborahShulman <shulman.deborah1@gmail.com>
2022-01-25 19:47:50 +0200

    got the action bar view with style

--- PrototypeWithAuth/wwwroot/ReactViews/floating-action-bar.jsx
+++ PrototypeWithAuth/wwwroot/ReactViews/floating-action-bar.jsx
@@ -1,5 +1,7 @@
 ﻿import React from 'react';
-import { Link, Router, Route, useHistory, Switch } from 'react-router-dom';
+import {
+    Link, Route, Switch, StaticRouter
+} from 'react-router-dom';
 
 export default class FloatingActionBar extends React.Component {
     constructor(props) {
@@ -11,34 +13,53 @@ export default class FloatingActionBar extends React.Component {
 
     }
 
+
+
     render() {
-       // let history = useHistory();
-        let location = useLocation();
-        let background = location.state && location.state.background;
+
         return (
 
-            <div>
-                <Router >
-                    <Switch location={background || location}>
-                        <Route path="/" component={React} />
-                        <Route path="/view/:postId" component={React} />
+                    <div className="floating-action-bar">
+                <StaticRouter >
+                    <Switch  >
+                        <Route path="/" />
                     </Switch>
-                </Router>
-                <ul className="list-group list-group-horizontal">
-                    <li className="list-group-item">
-                        <Link to="/" ><i className="icon-approve-24px"></i>Order</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/" ><i className="icon-description-24px2"></i>Add Quote</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/" ><i className="con-insert_drive_file-24px-1"></i>Add File</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/" ><i className="icon-delete-24px1"></i>Delete</Link>
-                    </li>
-                </ul>
-                </div>
+                    <div>
+                        
+                        <ul className="list-group list-group-horizontal">
+                            <li className="list-group-item ">
+                                <span>2 items selected</span>
+                            </li>
+                            <li className="list-group-item ">
+                                
+                            </li>
+                            <li className="list-group-item ">
+                                <Link to="/" ><i className="icon-approve-24px"></i>Order</Link>
+                            </li>
+                            <li className="list-group-item ">
+                                <Link to="/" ><i className="icon-description-24px2"></i>Add Quote</Link>
+                            </li>
+                            <li className="list-group-item ">
+                                <Link to="/" ><i className="icon-insert_drive_file-24px-1"></i>Add File</Link>
+                            </li>
+                            <li className="list-group-item ">
+                                <Link to="/" ><i className="icon-delete-24px1"></i>Delete</Link>
+                            </li>
+                            <li className="list-group-item ">
+
+                            </li>
+                            <li className="list-group-item ">
+                                <button type="button" className="close ">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </li>
+                        </ul>
+
+                      
+                    </div>
+                </StaticRouter >
+                    </div>
         );
     }
 }
+
