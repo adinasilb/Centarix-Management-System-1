c29d059aeb7b7f0f9fa680e1b0dbef8cbbf6c04c
DeborahShulman <shulman.deborah1@gmail.com>
2022-02-02 09:51:16 +0200

    deletemodal

--- PrototypeWithAuth/wwwroot/ReactViews/delete-modal.jsx
+++ PrototypeWithAuth/wwwroot/ReactViews/delete-modal.jsx
@@ -1,40 +1,46 @@
 ﻿import React, { Component } from 'react';
-import createPortal from 'react-dom';
+import ReactDOM from 'react-dom';
+import CloseButton from './close-button.jsx'
+import { openModal } from './modal-functions.jsx'
 export default class DeleteModal extends Component {
-    state = { isLoaded: false, view: "" };
+    state = { isLoaded: false, view: "", showModal:false };
 
-    htmlDecode(input) {
-        var e = document.createElement('div');
-        e.innerHTML = input;
-        return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+   
+    componentWillUnmount() {
+        console.log('unmount')
     }
-
+    componentDidUpdate() {
+        openModal("modal");       
+    }
     componentDidMount() {
-        console.log("here")
-        fetch("/Requests/DeleteModal?id=4", {
-            method: "GET"            
-        })
-            .then((response) => { return response.text(); })
-            .then(result => {
-                alert(result);
-                this.setState({
-                    view: result,
-                    isLoaded: true
+/*        if (this.state.isLoaded == true) {*/
+            fetch("/Requests/DeleteModal?id=4", {
+                method: "GET"
+            })
+                .then((response) => { return response.text(); })
+                .then(result => {
+                    this.setState({
+                        view: result,
+                        isLoaded: true,
+                        showModal: true
+                    });
                 });
-            });
+      /*  }*/
     }
 
     render() {
-        const { isLoaded } = this.state;
-
-        if (!isLoaded) {
-            return <div>Loading...</div>;
+        if (this.state.showModal == true) {
+            return ReactDOM.createPortal(<div><div className="close-button"><CloseButton/><div dangerouslySetInnerHTML={{ __html: this.state.view }} /></div></div>,
+                document.getElementsByClassName("delete-item")[0]
+            );
         }
-        return createPortal(<div dangerouslySetInnerHTML={{ __html: this.htmlDecode(this.state.view) }} />,
-        document.getElementById("deleteItem")
-        );
+        else {
+            return null;
+        }
+        
     }
 }
+
 //if (typeof window !== 'undefined') {
 //    ReactDOM.render(<DeleteModal />, document.getElementById("deleteItem"));
 //}
---
+++ PrototypeWithAuth/wwwroot/ReactViews/modal-functions.jsx
@@ -0,0 +1,16 @@
+﻿import ReactDOM, {  unmountComponentAtNode, findDOMNode } from "react-dom";
+
+var openModal = function (modalClass) {
+    var modalElement = document.getElementsByClassName(modalClass)[0];
+    if (modalElement != undefined) {
+        var myModal = new bootstrap.Modal(modalElement, {
+            backdrop: true,
+            keyboard: false,
+        });
+        myModal.show()
+    }
+    //ReactDOM.render(<CloseButton/>,
+    //    document.getElementsByClassName("close-button")[0]
+    //);
+}
+export { openModal };
