967e1252bbd5b1f18cecbf6ff47c3c2c934c65ea
DeborahShulman <shulman.deborah1@gmail.com>
2022-02-02 09:51:16 +0200

    deletemodal

---
+++ PrototypeWithAuth/wwwroot/ReactViews/close-button.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import { useLocation, useHistory } from "react-router-dom";
+
+
+export default function CloseButton() {
+    const history = useHistory();
+    var back = e => {
+         e.stopPropagation();
+         console.log("go back")
+        history.goBack();
+        console.log(history.location)
+    };
+
+    return (<button
+        className="close modal-close-padding modal-close-style"
+        onClick={back}>
+        <span aria-hidden="true">&times;</span>
+    </button>
+    );
+}
--- PrototypeWithAuth/wwwroot/ReactViews/expose-components.js
+++ PrototypeWithAuth/wwwroot/ReactViews/expose-components.js
@@ -4,6 +4,8 @@ import ReactDOMServer from 'react-dom/server';
 
 //import FloatingActionBar from './floating-action-bar.jsx'
 import RootComponent from './root-component.jsx';
+import { openModal } from './modal-functions.jsx'
+import CloseButton  from './close-button.jsx'
 
 global.React = React;
 global.ReactDOM = ReactDOM;
@@ -11,5 +13,7 @@ global.ReactDOMServer = ReactDOMServer;
 
 //global.FloatingActionBar = FloatingActionBar;
 global.RootComponent = RootComponent
+global.openModal = openModal;
+global.CloseButton = CloseButton;
 
 
