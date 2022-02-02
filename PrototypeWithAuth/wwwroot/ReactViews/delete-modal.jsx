2c00b87ac64dc1e670ee946090092f326237f6ee
DeborahShulman <shulman.deborah1@gmail.com>
2022-02-02 09:51:16 +0200

    deletemodal

--- PrototypeWithAuth/wwwroot/ReactViews/delete-modal.jsx
+++ PrototypeWithAuth/wwwroot/ReactViews/delete-modal.jsx
@@ -1,40 +1,46 @@
 ﻿import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import CloseButton from './close-button.jsx'
+import { openModal } from './modal-functions.jsx'
 export default class DeleteModal extends Component {
-    state = { isLoaded: false, view: "" };
+    state = { isLoaded: false, view: "", showModal:false };
 
+   
+    componentWillUnmount() {
+        console.log('unmount')
+    }
     componentDidUpdate() {
-        var myModal = new bootstrap.Modal(document.getElementsByClassName('modal')[0],  {
-            backdrop: true,
-            keyboard: false,
-        });
-        myModal.show()
+        openModal("modal");       
     }
     componentDidMount() {
-        fetch("/Requests/DeleteModal?id=4", {
-            method: "GET"            
-        })
-            .then((response) => { return response.text(); })
-            .then(result => {
-                alert(result);
-                this.setState({
-                    view: result,
-                    isLoaded: true
+/*        if (this.state.isLoaded == true) {*/
+            fetch("/Requests/DeleteModal?id=4", {
+                method: "GET"
+            })
+                .then((response) => { return response.text(); })
+                .then(result => {
+                    this.setState({
+                        view: result,
+                        isLoaded: true,
+                        showModal: true
+                    });
                 });
-            });
+      /*  }*/
     }
 
     render() {
-        const { isLoaded } = this.state;
-
-        if (!isLoaded) {
-            return <div>Loading...</div>;
+        if (this.state.showModal == true) {
+            return ReactDOM.createPortal(<div><div className="close-button"><CloseButton/><div dangerouslySetInnerHTML={{ __html: this.state.view }} /></div></div>,
+                document.getElementsByClassName("delete-item")[0]
+            );
         }
-        return ReactDOM.createPortal(<div dangerouslySetInnerHTML={{ __html: this.state.view }} />,
-        document.getElementById("deleteItem")
-        );
+        else {
+            return null;
+        }
+        
     }
 }
+
 //if (typeof window !== 'undefined') {
 //    ReactDOM.render(<DeleteModal />, document.getElementById("deleteItem"));
 //}
